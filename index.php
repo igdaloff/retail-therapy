5c605e254438d211198bc27b28fcca993a205580
igdaloff <louis.nathan@gmail.com>
2014-06-12 22:44:18 -0400

    adding a bunch of actual content

--- index.php
+++ index.php
@@ -5,27 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Retail Therapy</title>
     <link rel="stylesheet" href="css/screen.css">
+
+    <!-- Webfonts -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
   </head>
   <body>
 
-  <form class="main-form" action="dispatch.aspx" role="form">
+  <header class="wrapper">
     <h1>Retail Therapy</h1>
-    <ul>
-      <li>
-        <label>Address:</label>
-        <input class="form-control" type="text" name="address" />
-      </li>
-      <li>
-        <label>Zip:</label>
-        <input class="form-control" type="text" name="zip" />
-      </li>
-    </ul>
-      <input type="hidden" name="option1" value="123"/>
-      <input class="btn btn-primary" type="submit" value="Submit"/>
-  </form>
+    <img src="images/main-logo.png" alt="Retail Therapy logo" />
+  </header>
 
+  <div class="wrapper main-content">
+    <form class="main-form" action="dispatch.aspx" role="form">
+      <div class="form-instructions">
+        <h2>Find the best offers below!</h2>
+        <p>Enter your information</p>
+      </div>
+      <ul>
+        <li>
+          <label>Address:</label>
+          <input class="form-control" type="text" name="address" />
+        </li>
+        <li>
+          <label>Zip:</label>
+          <input class="form-control" type="text" name="zip" />
+        </li>
+      </ul>
+        <input type="hidden" name="option1" value="123"/>
+        <input class="btn" type="submit" value="Submit"/>
+    </form>
+  </div>
+
+  <footer>
+    <div class="wrapper">
+      <h4>Find offers from these great providers:</h4>
+      <ul>
+        <li><img src="images/att-logo.png" alt="AT&T Logo" /></li>
+        <li><img src="images/verizon-logo.png" alt="Verizon Logo" /></li>
+        <li><img src="images/tw-logo.png" alt="Time Warner Logo" /></li>
+        <li><img src="images/mediacom-logo.png" alt="Mediacom Logo" /></li>
+        <li><img src="images/charter-logo.png" alt="Charter Logo" /></li>
+        <li><img src="images/cox-logo.png" alt="Cox Logo" /></li>
+      </ul>
+      <div class="footer-disclaimer">
+        <small>**Reliant is not affiliated with any of the Phone, TV or Internet providers and is not responsible for any services purchased from these providers or the connection of these services. Actual connection will be performed at a future scheduled date by these providers, and Reliant is not responsible for scheduling and making any connections of otherwise establishing any service with these providers. Reliant does not warrant or endorse any phone, TV or Internet service offered through Reliant Connections and will not be responsible to participating customers for any such claims.</small>
+      </div>
+      <div class="footer-admin">
+        <img src="images/acceller.png" alt="Acceller" />
+        <small>&copy; 2014 Retail Therapy. All Rights Reserved.</small>
+      </div>
+    </div>
+  </footer>
    <script src="js/app.js"></script>
    <script src="js/vendors.js"></script>
   </body>
