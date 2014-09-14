a49b2c6ff9e5d65793db8ed952b467bad84dede4
igdaloff <louis.nathan@gmail.com>
2014-09-14 17:58:50 -0400

    responsiveness for form and terms page; also making php includes

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+  <footer>
+    <div class="wrapper">
+      <h4>Find offers from these great providers:</h4>
+      <ul>
+        <li><img src="/images/att-logo.png" alt="AT&T Logo" /></li>
+        <li><img src="/images/verizon-logo.png" alt="Verizon Logo" /></li>
+        <li><img src="/images/tw-logo.png" alt="Time Warner Logo" /></li>
+        <li><img src="/images/mediacom-logo.png" alt="Mediacom Logo" /></li>
+        <li><img src="/images/charter-logo.png" alt="Charter Logo" /></li>
+        <li><img src="/images/cox-logo.png" alt="Cox Logo" /></li>
+      </ul>
+      <div class="footer-disclaimer">
+        <small>**Reliant is not affiliated with any of the Phone, TV or Internet providers and is not responsible for any services purchased from these providers or the connection of these services. Actual connection will be performed at a future scheduled date by these providers, and Reliant is not responsible for scheduling and making any connections of otherwise establishing any service with these providers. Reliant does not warrant or endorse any phone, TV or Internet service offered through Reliant Connections and will not be responsible to participating customers for any such claims.</small>
+      </div>
+      <div class="footer-admin">
+        <img src="/images/acceller.png" alt="Acceller" />
+        <small>&copy; 2014 Retail Therapy. All Rights Reserved.</small>
+      </div>
+    </div>
+  </footer>
+   <script src="/js/app.js"></script>
+  </body>
+</html>
