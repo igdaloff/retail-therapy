522f3c95597eec3d139a8ceef62e8d2b8d67f2d2
igdaloff <louis.nathan@gmail.com>
2014-09-15 21:18:23 -0400

    putting signup form on own page

--- includes/header.php
+++ includes/header.php
@@ -20,49 +20,5 @@
   <header class="wrapper">
     <h1>Retail Therapy</h1>
     <a href="/" class="main-logo"><img src="/images/main-logo.png" alt="Retail Therapy logo" /></a>
-    <div class="signup-form-container">
-      <span class="signup-btn"><span>Sign Up</span></span>
-      <form id="form1" name="form1" class="signup-form" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://igdaloff.wufoo.com/forms/zity6i90wiien4/#public">
-        <ul>
-          <li id="foli114" class="form-row">
-            <label for="Field114">Name</label>
-            <div class="half">
-              <input id="Field117" name="Field117" type="text" class="input-field" value="" maxlength="255" tabindex="1" onkeyup="" required placeholder="Full name" />
-            </div>
-          </li>
-
-          <li id="foli8" class="form-row">
-            <label class="desc" id="title8" for="Field8">Email</label>
-            <input id="Field8" name="Field8" type="email" spellcheck="false" class="input-field text medium" value="" maxlength="255" tabindex="3" required placeholder="email@address.com"/>
-          </li>
-
-          <li id="foli9" class="phone form-row">
-            <label class="desc" id="title9" for="Field9">Phone Number</label>
-            <input id="Field9" name="Field9" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="4" required placeholder="###" />
-            <span class="symbol">-</span>
-            <input id="Field9-1" name="Field9-1" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="5" required placeholder="###" />
-            <span class="symbol">-</span>
-            <input id="Field9-2" name="Field9-2" type="tel" class="input-field text" value="" size="4" maxlength="4" tabindex="6" required placeholder="####" />
-          </li>
-
-          <li id="foli10" class="form-row">
-            <fieldset>
-              <input id="Field10" name="Field10" type="checkbox" class="checkbox" value="I have read the Terms of Agreement" tabindex="7" />
-              <label class="choice" for="Field10">I have read the <a href="/terms">Terms of Agreement</a></label>
-            </fieldset>
-          </li>
-
-          <li>
-            <input id="saveForm" name="saveForm" class="btn btn-white submit" type="submit" value="Submit" />
-          </li>
-
-          <li class="hide">
-            <label for="comment">Do Not Fill This Out</label>
-            <textarea name="comment" id="comment" rows="1" cols="1"></textarea>
-            <input type="hidden" id="idstamp" name="idstamp" value="CCnJCfzeRRWUsnb3CBBDM90MueoS+zgiQGw/b1iGYqE=" />
-          </li>
-        </ul>
-      </form>
-      <span class="close-signup-form">&times;</span>
-    </div>
+    <a href="/signup" class="signup-btn btn">Sign Up</a>
   </header>
