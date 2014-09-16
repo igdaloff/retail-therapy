8e9493b84ce5a52004b7ed6e4a11381f9d66180d
igdaloff <louis.nathan@gmail.com>
2014-09-15 21:36:39 -0400

    hooking up form to adams account

--- signup/index.php
+++ signup/index.php
@@ -1,35 +1,35 @@
 <?php include("../includes/header.php"); ?>
 
 <div class="wrapper main-content terms">
-	<h2>Sign Up for Retail Therapy</h2>
+	<h3>Sign Up for Retail Therapy</h3>
 	<div class="signup-form-container">
-	  <form id="form1" name="form1" class="signup-form" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://igdaloff.wufoo.com/forms/zity6i90wiien4/#public">
+	  <form id="form1" name="form1" class="signup-form" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://tpagold.wufoo.com/forms/z159wtln1fccju4/#public">
 	    <ul>
-	      <li id="foli114" class="form-row">
-	        <label for="Field114">Name</label>
+	      <li id="foli1" class="form-row">
+	        <label for="Field1">Name</label>
 	        <div class="half">
-	          <input id="Field117" name="Field117" type="text" class="input-field" value="" maxlength="255" tabindex="1" onkeyup="" required placeholder="Full name" />
+	          <input id="Field1" name="Field1" type="text" class="input-field" value="" maxlength="255" tabindex="1" onkeyup="" required placeholder="Full name" />
 	        </div>
 	      </li>
 
-	      <li id="foli8" class="form-row">
-	        <label class="desc" id="title8" for="Field8">Email</label>
-	        <input id="Field8" name="Field8" type="email" spellcheck="false" class="input-field text medium" value="" maxlength="255" tabindex="3" required placeholder="email@address.com"/>
+	      <li id="foli2" class="form-row">
+	        <label class="desc" id="title2" for="Field2">Email</label>
+	        <input id="Field2" name="Field2" type="email" spellcheck="false" class="input-field text medium" value="" maxlength="255" tabindex="3" required placeholder="email@address.com"/>
 	      </li>
 
-	      <li id="foli9" class="phone form-row">
-	        <label class="desc" id="title9" for="Field9">Phone Number</label>
-	        <input id="Field9" name="Field9" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="4" required placeholder="###" />
+	      <li id="foli3" class="phone form-row">
+	        <label class="desc" id="title3" for="Field9">Phone Number</label>
+	        <input id="Field3" name="Field3" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="4" required placeholder="###" />
 	        <span class="symbol">-</span>
-	        <input id="Field9-1" name="Field9-1" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="5" required placeholder="###" />
+	        <input id="Field3-1" name="Field3-1" type="tel" class="input-field text" value="" size="3" maxlength="3" tabindex="5" required placeholder="###" />
 	        <span class="symbol">-</span>
-	        <input id="Field9-2" name="Field9-2" type="tel" class="input-field text" value="" size="4" maxlength="4" tabindex="6" required placeholder="####" />
+	        <input id="Field3-2" name="Field3-2" type="tel" class="input-field text" value="" size="4" maxlength="4" tabindex="6" required placeholder="####" />
 	      </li>
 
-	      <li id="foli10" class="form-row">
+	      <li id="foli4" class="form-row">
 	        <fieldset>
-	          <input id="Field10" name="Field10" type="checkbox" class="checkbox" value="I have read the Terms of Agreement" tabindex="7" />
-	          <label class="choice" for="Field10">I have read the <a href="/terms">Terms of Agreement</a></label>
+	          <input id="Field4" name="Field4" type="checkbox" class="checkbox" value="I have read the Terms of Agreement" tabindex="7" />
+	          <label class="choice" for="Field4">I have read the <a href="/terms">Terms of Agreement</a></label>
 	        </fieldset>
 	      </li>
 
@@ -40,7 +40,7 @@
 	      <li class="hide">
 	        <label for="comment">Do Not Fill This Out</label>
 	        <textarea name="comment" id="comment" rows="1" cols="1"></textarea>
-	        <input type="hidden" id="idstamp" name="idstamp" value="CCnJCfzeRRWUsnb3CBBDM90MueoS+zgiQGw/b1iGYqE=" />
+	        <input type="hidden" id="idstamp" name="idstamp" value="5Hs7BCfydBnWffmYYabn8KyB/gVoEehAmyteJ0bPc0w=" />
 	      </li>
 	    </ul>
 	  </form>
